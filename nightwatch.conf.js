061c8e632485efc2f7c17763cf941ffffd120b2f
Divya <divya.itsofficial@gmail.com>
2020-02-03 17:00:57 +0530

    initial commit

---
+++ nightwatch.conf.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'src_folders': ['tests'],
+  'webdriver': {
+      'start_process': true,
+      'server_path': 'node_modules/.bin/chromedriver',
+      'port': 9515
+  },
+  'test_settings': {
+      'default': {
+          'screenshots': {
+              'enabled': true,
+              'on_failure': true,
+              'on_error': true,
+              'path': 'tests_output/screenshots'
+          },
+          'desiredCapabilities': {
+              'browserName': 'chrome',
+          }
+      }
+  }
+};
